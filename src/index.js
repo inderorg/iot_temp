6e1417a228ac3bf5add82ccbaf0d923366362f5d
inder.patliya <inder.patliya@247.ai>
2019-09-28 15:25:47 +0530

    deleted

--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ import rootReducer from './reducers';
 import { fetchDeviceData } from './actions/index';
 import './index.css';
 import App from './App';
+
+let initialState = [{id:1,name:'John Doe',grade:1,school:'React Redux School'},{id:2,name:'Jane Doe',grade:2,school:'React Redux School'}
+,{id:3,name:'Terry Adams',grade:3,school:'React Redux School'},{id:4,name:'Jenny Smith',grade:4,school:'React Redux School'}];
+
+if( localStorage.getItem("devices") === null)
+localStorage.setItem('devices',JSON.stringify(initialState));
+else 
+initialState = JSON.parse(localStorage.getItem('devices'));
+
 const store = createStore(rootReducer, applyMiddleware(thunk));
 store.dispatch(fetchDeviceData());
 
